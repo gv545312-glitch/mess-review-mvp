b337cae292af602652f7f49e33b80d7e7ce3dd82
Mac Air <macair@Macs-MacBook-Air.local>
2026-05-24 17:12:56 +0530

    fixed universities section

--- app.py
+++ app.py
@@ -50,26 +50,25 @@ def get_all_reviews():
 universities = [
     {
         "name": "Amity University",
-        "image": "https://images.unsplash.com/photo-1562774053-701939374585"
+        "image": "https://images.unsplash.com/photo-1562774053-701939374585?w=600"
     },
     {
         "name": "Galgotias University",
-        "image": "https://images.unsplash.com/photo-1523050854058-8df90110c9f1"
+        "image": "https://images.unsplash.com/photo-1562774053-701939374585?w=600"
     },
     {
         "name": "Sharda University",
-        "image": "https://images.unsplash.com/photo-1498243691581-b145c3f54a5a"
+        "image": "https://images.unsplash.com/photo-1498243691581-b145c3f54a5a?w=600"
     },
     {
         "name": "Bennett University",
-        "image": "https://images.unsplash.com/photo-1541339907198-e08756dedf3f"
+        "image": "https://images.unsplash.com/photo-1541339907198-e08756dedf3f?w=600"
     },
     {
         "name": "JIMS Noida",
-        "image": "https://images.unsplash.com/photo-1564981797816-1043664bf78d"
+        "image": "https://images.unsplash.com/photo-1564981797816-1043664bf78d?w=600"
     }
 ]
-
 
 # ---------------- ROUTES ---------------- #
 
