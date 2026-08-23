dc85afc884fdb4b5ba50ec2f5511cd62fa1b1474
Mac Air <macair@Macs-MacBook-Air.local>
2026-08-23 19:46:11 +0530

    added YMCA University Faridabad

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ def init_db():
 init_db()
 
 universities = [
+    {"name": "YMCA University of Science and Technology", "city": "Faridabad, Haryana", "image": "/static/images/chitkara.jpg"},
     {"name": "K.R. Mangalam University", "city": "Gurugram, Haryana", "image": "/static/images/chitkara.jpg"},
     {"name": "CGC University", "city": "Mohali, Punjab", "image": "/static/images/chitkara.jpg"},
     {"name": "NSUT Delhi", "city": "New Delhi", "image": "/static/images/nsut.jpg"},
