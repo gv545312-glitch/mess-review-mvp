8244a1581a3b50b43db9505d725c7892a565116e
Mac Air <macair@Macs-MacBook-Air.local>
2026-08-29 13:38:58 +0530

    fix netlify function handler

--- netlify/functions/app.py
+++ netlify/functions/app.py
@@ -1,16 +1,9 @@
-import json
 import sys
 import os
-
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..'))
 
+from app import app
+import serverless_wsgi
+
 def handler(event, context):
-    try:
-        import serverless_wsgi
-        from app import app
-        return serverless_wsgi.handle_request(app, event, context)
-    except Exception as e:
-        return {
-            "statusCode": 500,
-            "body": str(e)
-        }
+    return serverless_wsgi.handle_request(app, event, context)
